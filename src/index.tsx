50c93de9963cfb29ce6510f081d672e4725cf5b2
Kitsunaana <selimkhanov_deni@list.ru>
2024-07-07 11:47:43 +0700

    build: add react library

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+const App = () => {
+    return <div></div>
+}
+
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
